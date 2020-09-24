d32e597419838ce480277a42092eb6af929fcee0
Tajveez <tajveez786@gmail.com>
2020-09-24 22:58:32 +0500

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,18 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import { Container, Form } from "react-bootstrap";
 export default function Login() {
-  return <div>Login</div>;
+  const idRef = useRef();
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: "100vh" }}
+    >
+      <Form className="w-100">
+        <Form.Group>
+          <Form.Label>Enter your Id</Form.Label>
+          <Form.Control type="text" ref={idRef}></Form.Control>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 }
