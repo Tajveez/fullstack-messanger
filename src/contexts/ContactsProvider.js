902a9b7587e18907df3fb01b0af75a57caf3981f
Tajveez <tajveez786@gmail.com>
2020-09-27 17:19:10 +0500

    new contact modal form

---
+++ src/contexts/ContactsProvider.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const ContactsContext = React.createContext();
+export function ContactsProvider({ childern }) {
+  const [contacts, setContacts] = useLocalStorage("contacts", []);
+
+  function createContact(id, name) {
+    setContacts((prevContacts) => {
+      return [...prevContacts, { id, name }];
+    });
+  }
+  // https://youtu.be/tBr-PybP_9c?t=2387
+  return (
+    <ContactsContext.Provider value={{ contacts, createContact }}>
+      {childern}
+    </ContactsContext.Provider>
+  );
+}
