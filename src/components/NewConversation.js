1060f9f409512bb810c55a777a8cde7cdd9d8e8d
Tajveez <tajveez786@gmail.com>
2020-10-04 23:52:18 +0500

    master# changes

--- src/components/NewConversation.js
+++ src/components/NewConversation.js
@@ -1,15 +1,41 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Form, Modal, Button } from "react-bootstrap";
+import { useContacts } from "../contexts/ContactsProvider";
 
 export default function NewConversation({ closeModal }) {
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+  const { contacts } = useContacts();
+  function handleCheckboxChange(contactId) {
+    setSelectedContactIds((prevIds) => {
+      if (prevIds.includes(contactId)) {
+        return prevIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      } else {
+        return [...prevIds, contactId];
+      }
+    });
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
   }
+
   return (
     <>
       <Modal.Header closeButton>Create New Conversation</Modal.Header>
       <Modal.Body>
         <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group controlId={contact.id} key={contact.id}>
+              <Form.Check
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckboxChange(contact.id)}
+              />
+            </Form.Group>
+          ))}
           <Form.Group></Form.Group>
           <Button>Submit</Button>
         </Form>
