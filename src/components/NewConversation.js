05a9d47e58fd1f9086b6e263ba5f6d4bc3f97b9d
Tajveez <tajveez786@gmail.com>
2020-10-04 23:53:41 +0500

    master# changes

--- src/components/NewConversation.js
+++ src/components/NewConversation.js
@@ -19,6 +19,8 @@ export default function NewConversation({ closeModal }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    // createConversation...
+    closeModal();
   }
 
   return (
