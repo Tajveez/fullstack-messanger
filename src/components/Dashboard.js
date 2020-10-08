1ba762dae8ff4a45ce3b6768024c4fc7f21a4af6
Tajveez <tajveez786@gmail.com>
2020-10-08 23:37:42 +0500

    code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import { useConversations } from "../contexts/ConversationsProvider";
+import OpenConversation from "./OpenConversation";
 
 export default function Dashboard({ id }) {
+  const { selectedConversation } = useConversations();
+  console.log(selectedConversation);
   return (
     <div className="d-flex" style={{ height: "100vh" }}>
       <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
     </div>
   );
 }
---
+++ src/components/OpenConversation.js
@@ -0,0 +1,24 @@
+import React, { useRef, useState } from "react";
+import { Form, InputGroup } from "react-bootstrap";
+export default function OpenConversation() {
+  const [text, setText] = useState("");
+  return (
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto"></div>
+      <Form>
+        <Form.Group className="m-2">
+          <InputGroup>
+            <Form.Control
+              required
+              as="textarea"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: "75px", resize: "none" }}
+              placeHolder="Enter Your Message Here."
+            ></Form.Control>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
